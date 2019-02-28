0fb18f06454a0d39cd554156eed212039ff5c02b
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-02-28 14:40:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ def valid_move?(board, index)
 end
 
 def turn(board) 
-  until over?(board)
+  until over?(board) end
   puts 'Please enter 1-9:'
   user_input = gets.strip
   index = input_to_index(user_input)
